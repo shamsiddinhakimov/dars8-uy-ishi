ee5746ef5105265da45affc20657bb3fa3f630e5
shamsiddinhakimov <shamsiddinhakimov@gmail.com>
2024-02-10 15:21:30 +0500

    assasas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 
 import './App.css'
 
-
+import { useRef,useEffect,useState } from 'react'
 function App() {
-  
+  const inputRef = useRef('')
+  if(inputRef.current.valueOf.trim().lenght < 3){
+    alert('3 ta dan kam belgi kiritilmasin')
+    inputRef.current.focus()
+    return;
+  }
+
+  const todo = {
+    id: Date.now(),
+    name:inputRef.current.value()
+  }
+  let data = [];
+  if(localStorage.getItem('todos')){
+    data = JSON.parse(localStorage.getItem('todos'));
+  }
+
+  data.push(todo)
+  localStorage.setItem('todos',JSON.stringify(data))
+  inputRef.current.value ='';
+
+
   return (
     <>
    <h1 className='h1'>Todo app</h1>
     <div className="todo-wrapper">
-    <form>
-    <input type="text" />
+    <form onSubmit={handleSubmit}>
+    <input ref={inputRef} type="text" />
    <button>OK</button>
     </form>
     <br />
