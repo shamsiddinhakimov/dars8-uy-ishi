7e0ac8c9c6a4c0ecf99a241a2fc7016f77a2bc98
shamsiddinhakimov <shamsiddinhakimov@gmail.com>
2024-02-10 14:53:37 +0500

    saas

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,21 @@ function App() {
   
   return (
     <>
-   
-  
+   <h1>Todo app</h1>
+   <input type="text" />
+   <button>OK</button>
+    <br />
+    <ul>
+      <li>
+        <input type="text" />
+        <span>Kitob oqish</span>
+        <span>
+        <span>update</span>
+        <span>delete</span>
+
+        </span>
+      </li>
+    </ul>
     
     
    
